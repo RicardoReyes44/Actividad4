95c1a2fbee845fb1891764a5e7afecc3880f2de0
RicardoReyes44 <rickyboy1002@gmail.com>
2020-09-23 23:01:43 -0500

    Se agregaron los metodo estaticos 'crearConjuntoDivisor' y 'crearConjuntoPrimo' a la clase de prueba

--- src/PruebaConjunto.java
+++ src/PruebaConjunto.java
@@ -3,6 +3,45 @@ import java.util.Random;
 
 public class PruebaConjunto {
 
+	public static int[] crearConjuntoPrimo() {
+
+		int[] array = new int[25];
+		int pos = 0;
+		
+		for(int i=0; i<=100; i++) {
+			int cont = 0;
+
+			for(int j=1; j<=i; j++) {
+				if(i%j==0) {
+					cont++;
+				}
+			}
+
+			if(cont==2) {
+				array[pos] = i;
+				pos++;
+			}
+		}
+		return array;	
+	}
+
+
+	public static int[] crearConjuntoDivisor() {
+
+		int[] array = new int[12];
+		int pos = 0;
+		
+		for(int i=1; i<=350; i++) {
+			if(350%i==0) {
+				array[pos] = i;
+				pos++;
+			}
+		}
+
+		return array;
+	}
+
+
     public static void main(String[] args) {
 		
 		
